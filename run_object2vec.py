8b3e6c8375fa68033ce4588be469fd1251d64233
EricElmoznino <eric.elmoznino@gmail.com>
2021-04-06 17:24:20 -0400

    - Fixed a bug where I wasn't assigning the result of a return value - Added Object2Vec

--- run_object2vec.py
+++ run_object2vec.py
@@ -1,17 +1,19 @@
 import argparse
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
+
 import pandas as pd
 import tensorflow as tf
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
-import brainscore.benchmarks as bench
-from brainscore.metrics.regression import linear_regression
+from bonnerlab_brainscore.benchmarks.object2vec import Object2VecEncoderBenchmark
 from dimensionality import get_results
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Run unsupervised models on MajajHong2015.')
+    parser = argparse.ArgumentParser(description='Run unsupervised models on Object2Vec.')
     parser.add_argument('--debug', action='store_true',
                         help='run only a single model and layer')
+    parser.add_argument('--data_dir', type=str, required=True,
+                        help='path to the Object2Vec dataset directory')
     parser.add_argument('--regression', type=str, default='pls',
                         help='regression type for fitting neural data', choices=['pls', 'lin'])
     parser.add_argument('--n_components', type=int, default=1000,
@@ -20,23 +22,15 @@ if __name__ == '__main__':
 
     if args.debug:
         models = ['resnet18-supervised']
-        regions = ['IT']
     else:
         models = None               # all models
-        regions = ['IT', 'V4']
     if args.n_components == -1:
         args.n_components = None    # no PCA
 
-    results = pd.DataFrame()
-    for region in regions:
-        benchmark_identifier = f'dicarlo.MajajHong2015public.{region}-pls'
-        benchmark = bench.load(benchmark_identifier)
-        if args.regression == 'lin':
-            benchmark._identifier = benchmark.identifier.replace('pls', args.regression)
-            benchmark._similarity_metric.regression = linear_regression()
+    benchmark = Object2VecEncoderBenchmark(data_dir=args.data_dir, regression=args.regression,
+                                           regions=['EVC', 'OPA', 'PPA', 'LOC', 'PFS', 'OFA', 'FFA'])
 
-        result = get_results(benchmark, args.n_components, models=models)
-        results = result.assign(region=region)
-        results = results.append(result)
+    results = get_results(benchmark, args.n_components, models=models)
+    results = results.assign(region='+'.join(benchmark.regions))
 
-    results.to_csv(f'results_majajhong2015_{args.regression}.csv', index=False)
+    results.to_csv(f'results_object2vec_{args.regression}.csv', index=False)
