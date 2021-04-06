190c717f40f7d853d47ef48490bb29a12aee9a6e
EricElmoznino <eric.elmoznino@gmail.com>
2021-04-06 16:46:50 -0400

    Refactored code to support multiple benchmarks

--- dimensionality.py
+++ dimensionality.py
@@ -1,15 +1,11 @@
-import argparse
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
-import itertools
 from sklearn.decomposition import PCA
 import pandas as pd
 import tensorflow as tf
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
-import brainscore.benchmarks as bench
-from brainscore.metrics.regression import linear_regression
 from candidate_models.base_models.unsupervised_vvs import ModelBuilder
 from model_tools.activations.pca import LayerPCA
 from model_tools.brain_transformation.neural import LayerScores
@@ -28,38 +24,63 @@ prednet_layers = ['A_%i' % i for i in range(1, 4)] \
 vvs_models = ['resnet18-supervised', 'resnet18-la', 'resnet18-ir', 'resnet18-ae',
               'resnet18-cpc', 'resnet18-color', 'resnet18-rp', 'resnet18-depth',
               'resnet18-simclr', 'resnet18-deepcluster', 'resnet18-cmc']
-regions = ['IT', 'V4']
 
 
-def get_model_scores(model_identifier, regression, layers, region, n_components):
+def get_results(benchmark, n_components, models=None):
+    if models is None:
+        models = vvs_models
+
+    try:
+        stimulus_identifier = benchmark._assembly.stimulus_set_identifier
+    except AttributeError:
+        stimulus_identifier = benchmark.stimulus_set.identifier
+
+    results = pd.DataFrame()
+
+    for model_identifier in models:
+        if model_identifier in ModelBuilder.PT_MODELS:
+            layers = pt_resnet18_layers
+        elif model_identifier == 'prednet':
+            layers = prednet_layers
+        elif model_identifier == 'resnet18-simclr':
+            layers = tf_res18_layers[1:]
+        else:
+            layers = tf_res18_layers
+
+        result = get_model_scores(model_identifier, benchmark, layers, n_components)
+        effdims_dataset = get_model_effdims_dataset(model_identifier, n_components, stimulus_identifier)
+        result = pd.merge(result, effdims_dataset, on=['model', 'layer'])
+        if n_components is not None:
+            effdims_imagenet = get_model_effdims_imagenet(model_identifier, n_components)
+            result = pd.merge(result, effdims_imagenet, on=['model', 'layer'])
+
+        results = results.append(result)
+
+    return results
+
+
+def get_model_scores(model_identifier, benchmark, layers, n_components):
     tf.reset_default_graph()
 
     activations_model = ModelBuilder()(model_identifier)
-    activations_model.identifier += '-majajhong2015'
     if n_components is not None:
         activations_model.identifier += f'-{n_components}components'
         _ = LayerPCA.hook(activations_model, n_components=n_components)
 
-    benchmark_identifier = f'dicarlo.MajajHong2015public.{region}-pls'
-    benchmark = bench.load(benchmark_identifier)
-    if regression == 'lin':
-        benchmark._identifier = benchmark.identifier.replace('pls', regression)
-        benchmark._similarity_metric.regression = linear_regression()
-
     model_scores = LayerScores(model_identifier=activations_model.identifier,
                                activations_model=activations_model,
                                visual_degrees=8)
-    score = model_scores(benchmark=benchmark, layers=layers)
+    score = model_scores(benchmark=benchmark, layers=layers, prerun=True)
 
     score = score.to_dataframe(name='').unstack(level='aggregation').reset_index()
     score.columns = ['layer', 'score', 'score_error']
-    score = score.assign(model=model_identifier, region=region)
+    score = score.assign(model=model_identifier)
 
     return score
 
 
 def get_model_effdims_imagenet(model_identifier, n_components):
-    activations_model_identifier = model_identifier + '-majajhong2015'
+    activations_model_identifier = model_identifier
     if n_components is not None:
         activations_model_identifier = activations_model_identifier + f'-{n_components}components'
     function_identifier = f'{LayerPCA.__module__}.{LayerPCA.__name__}._pcas/' \
@@ -75,19 +96,18 @@ def get_model_effdims_imagenet(model_identifier, n_components):
     return effdims
 
 
-def get_model_effdims_dataset(model_identifier, n_components):
-    activations_model_identifier = model_identifier + '-majajhong2015'
+def get_model_effdims_dataset(model_identifier, n_components, stimuli_identifier):
+    activations_model_identifier = model_identifier
     if n_components is not None:
         activations_model_identifier = activations_model_identifier + f'-{n_components}components'
-    stimuli_identifier = 'dicarlo.hvm-public'
     function_identifier = 'model_tools.activations.core.ActivationsExtractorHelper._from_paths_stored/' \
                           f'identifier={activations_model_identifier},stimuli_identifier={stimuli_identifier}'
     store = store_xarray(identifier_ignore=['model', 'benchmark', 'layers', 'prerun'],
                          combine_fields={'layers': 'layer'})
     regressors = store.load(function_identifier)
     pcas = {layer: PCA().fit(x) for layer, x in regressors.groupby('layer')}
-    effdims = {layer: effective_dimensionality(pca) for layer, pca in pcas.items()}
 
+    effdims = {layer: effective_dimensionality(pca) for layer, pca in pcas.items()}
     effdims = [{'layer': layer, 'effective_dimensionality_dataset': dim} for layer, dim in effdims.items()]
     effdims = pd.DataFrame(effdims)
     effdims = effdims.assign(model=model_identifier)
@@ -99,44 +119,3 @@ def effective_dimensionality(pca):
     eigen_values = pca.singular_values_ ** 2 / (pca.n_components_ - 1)
     effective_dim = eigen_values.sum() ** 2 / (eigen_values ** 2).sum()
     return effective_dim
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Run unsupervised models on BrainScore.')
-    parser.add_argument('--debug', action='store_true',
-                        help='run only a single model and layer')
-    parser.add_argument('--regression', type=str, default='pls',
-                        help='regression type for fitting neural data', choices=['pls', 'lin'])
-    parser.add_argument('--n_components', type=int, default=1000,
-                        help='number of PCA components prior to fitting encoder (-1 for no PCA)')
-    args = parser.parse_args()
-
-    if args.debug:
-        vvs_models = ['resnet18-supervised']
-        regions = ['IT']
-    if args.n_components == -1:
-        args.n_components = None
-
-    results = pd.DataFrame()
-    for model_identifier, region in itertools.product(vvs_models, regions):
-        if model_identifier in ModelBuilder.PT_MODELS:
-            layers = pt_resnet18_layers
-        elif model_identifier == 'prednet':
-            layers = prednet_layers
-        elif model_identifier == 'resnet18-simclr':
-            layers = tf_res18_layers[1:]
-        else:
-            layers = tf_res18_layers
-        if args.debug:
-            layers = layers[:1]
-
-        result = get_model_scores(model_identifier, args.regression, layers, region, args.n_components)
-        effdims_dataset = get_model_effdims_dataset(model_identifier, args.n_components)
-        result = pd.merge(result, effdims_dataset, on=['model', 'layer'])
-        if args.n_components is not None:
-            effdims_imagenet = get_model_effdims_imagenet(model_identifier, args.n_components)
-            result = pd.merge(result, effdims_imagenet, on=['model', 'layer'])
-
-        results = results.append(result)
-
-    results.to_csv(f'results_{args.regression}.csv', index=False)
