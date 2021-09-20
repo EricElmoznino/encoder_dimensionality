067e814452d66884bf62663964f931ea4628c899
EricElmoznino <eric.elmoznino@gmail.com>
2021-09-20 16:08:17 -0400

    - Added code and scripts for intrinsic dimensionality - Split manifold code into two separate files

--- custom_model_tools/intrinsic_dim.py
+++ custom_model_tools/intrinsic_dim.py
@@ -2,17 +2,15 @@ from __future__ import annotations
 import logging
 import os.path
 from result_caching import store_dict
-from sklearn.decomposition import PCA
-from scipy.spatial.distance import euclidean
 import pandas as pd
 import numpy as np
-from tqdm import tqdm
 from brainio_base.assemblies import NeuroidAssembly
 from model_tools.activations.core import flatten
 from model_tools.utils import fullname
 from custom_model_tools.hooks import GlobalMaxPool2d, RandomProjection
+from lib.twoNN import TwoNearestNeighbors
 from utils import id_to_properties, get_imagenet_val
-from typing import List, Tuple
+from typing import List, Union
 
 
 class IntrinsicDimBase:
@@ -41,35 +39,156 @@ class IntrinsicDimBase:
 
     @store_dict(dict_key='layers', identifier_ignore=['layers'])
     def _fit(self, identifier, stimuli_identifier, layers, pooling):
-        concept_paths = self.get_image_concept_paths()
+        image_paths = self.get_image_paths()
+        if isinstance(image_paths[0], list):
+            cat_sizes = [0] + [len(cat) for cat in image_paths]
+            cat_sizes = np.cumsum(cat_sizes)
+            image_paths = [img for cat in image_paths for img in cat]
+        else:
+            cat_sizes = None
 
-        # Compute manifold geometry statistics for every layer individually to save on memory.
+        # Compute intrinsic dimensionality for every layer individually to save on memory.
         # This is more inefficient because we run images through the network several times,
         # but it is a more scalable approach when using many images and large layers.
-        layer_manifold_statistics = {}
+        layer_dims = {}
         for layer in layers:
             if pooling:
                 handle = GlobalMaxPool2d.hook(self._extractor)
             else:
                 handle = RandomProjection.hook(self._extractor)
 
-            self._logger.debug('Computing concept manifold geometries')
-            concept_manifolds = []
-            for stimuli_paths in tqdm(concept_paths, desc='concept manifolds'):
-                activations = self._extractor(stimuli_paths, layers=[layer])
-                activations = activations.sel(layer=layer).values
-                activations = flatten(activations)
-                concept_manifolds.append(ManifoldGeometry(activations))
+            self._logger.debug('Retrieving activations')
+            activations = self._extractor(image_paths, layers=layers)
+            activations = flatten(activations)
+            activations = activations.sel(layer=layer).values
 
-            self._logger.debug('Computing concept manifold statistics')
-            progress = tqdm(total=1, desc="manifold statistics")
-            layer_manifold_statistics[layer] = get_manifold_statistics(concept_manifolds)
-            progress.update(1)
-            progress.close()
+            two_nn = TwoNearestNeighbors()
+
+            self._logger.debug('Computing ID')
+            dim_global = two_nn.fit(activations).dim_
+
+            if cat_sizes is not None:
+                self._logger.debug('Computing inter/intra-class ID')
+                cat_means = []
+                dims_intra = []
+                for i in range(len(cat_sizes) - 1):
+                    cat_activations = activations[cat_sizes[i]:cat_sizes[i + 1]]
+                    cat_means.append(cat_activations.mean(axis=0))
+                    cat_dim_intra = two_nn.fit(cat_activations).dim_
+                    dims_intra.append(cat_dim_intra)
+                dims_intra = np.stack(dims_intra)
+                dim_intra_mean, dim_intra_std = dims_intra.mean(axis=0), dims_intra.std(axis=0)
+                dim_inter = two_nn.fit(np.stack(cat_means)).dim_
+
+            if cat_sizes is None:
+                layer_dims[layer] = {'ID': dim_global}
+            else:
+                layer_dims[layer] = {'ID': dim_global, 'inter-ID': dim_inter, 'intra-ID': dim_intra_mean,
+                                     'intra-ID (std)': dim_intra_std}
 
             handle.remove()
 
-        return layer_manifold_statistics
+        return layer_dims
 
-    def get_image_concept_paths(self) -> List[List[str]]:
+    def get_image_paths(self) -> Union[List[str], List[List[str]]]:
         raise NotImplementedError()
+
+
+class IntrinsicDimImageNet(IntrinsicDimBase):
+
+    def __init__(self, *args, num_classes=100, num_per_class=100, **kwargs):
+        super().__init__(*args, **kwargs, 
+                         stimuli_identifier='imagenet')
+        assert 2 <= num_classes <= 1000 and 2 <= num_per_class <= 50
+        self.num_classes = num_classes
+        self.num_per_class = num_per_class
+        self.image_paths = get_imagenet_val(num_classes, num_per_class, separate_classes=True)
+
+    def get_image_paths(self) -> List[List[str]]:
+        return self.image_paths
+
+
+class IntrinsicDimImageFolder(IntrinsicDimBase):
+
+    def __init__(self, data_dir, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        assert os.path.exists(data_dir)
+        self.data_dir = data_dir
+
+        image_paths = []
+        categories = os.listdir(self.data_dir)
+        for cat in categories:
+            cat_dir = os.path.join(self.data_dir, cat)
+            files = os.listdir(cat_dir)
+            paths = [os.path.join(cat_dir, file) for file in files]
+            image_paths.append(paths)
+        self.image_paths = image_paths
+
+    def get_image_paths(self) -> List[List[str]]:
+        return self.image_paths
+
+
+class IntrinsicDimImageNet21k(IntrinsicDimImageFolder):
+
+    def __init__(self, *args, num_classes=100, num_per_class=100, **kwargs):
+        super().__init__(*args, **kwargs, 
+                         stimuli_identifier='imagenet21k')
+        self.num_classes = num_classes
+        self.num_per_class = num_per_class
+
+        assert len(self.image_paths) >= num_classes
+        self.image_paths = self.image_paths[:num_classes]
+        for i in range(len(self.image_paths)):
+            assert len(self.image_paths[i]) >= num_per_class
+            self.image_paths[i] = self.image_paths[i][:num_classes]
+
+
+class IntrinsicDimObject2Vec(IntrinsicDimImageFolder):
+
+    def __init__(self, data_dir, *args, **kwargs):
+        data_dir = os.path.join(data_dir, 'stimuli_rgb')
+        super().__init__(data_dir, *args, **kwargs,
+                         stimuli_identifier='object2vec')
+
+
+class IntrinsicDimMajajHong2015(IntrinsicDimBase):
+    # Brainscore IT benchmark images (64 objects, 50 images/object)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs,
+                         stimuli_identifier='dicarlo.hvm-public')
+
+        data_dir = os.getenv('BRAINIO_HOME', os.path.expanduser('~/.brainio'))
+        data_dir = os.path.join(data_dir, 'image_dicarlo_hvm-public')
+        assert os.path.exists(data_dir)
+
+        image_paths = pd.read_csv(os.path.join(data_dir, 'image_dicarlo_hvm-public.csv'))
+        image_paths = image_paths[['object_name', 'filename']]
+        image_paths['filename'] = data_dir + '/' + image_paths['filename']
+        image_paths = image_paths.groupby('object_name')['filename'].agg(list)
+        image_paths = image_paths.values.tolist()
+        self.image_paths = image_paths
+
+    def get_image_image_paths(self) -> List[List[str]]:
+        return self.image_paths
+
+
+def neural_assembly_intrinsic_dim(assembly: NeuroidAssembly, cat_coord: str):
+    two_nn = TwoNearestNeighbors()
+
+    dim_global = two_nn.fit(assembly.values).dim_
+
+    activations = [g[1].values for g in assembly.groupby(cat_coord)]
+    cat_means = []
+    dims_intra = []
+    for cat_activations in activations:
+        cat_means.append(cat_activations.mean(axis=0))
+        cat_dim_intra = two_nn.fit(cat_activations).dim_
+        dims_intra.append(cat_dim_intra)
+    dims_intra = np.stack(dims_intra)
+    dim_intra_mean, dim_intra_std = dims_intra.mean(axis=0), dims_intra.std(axis=0)
+    dim_inter = two_nn.fit(np.stack(cat_means)).dim_
+
+    return {'ID': dim_global, 'inter-ID': dim_inter, 'intra-ID': dim_intra_mean,
+            'intra-ID (std)': dim_intra_std}
---
+++ scripts/compute_intrinsic_dim.py
@@ -0,0 +1,58 @@
+import argparse
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+import pandas as pd
+import tensorflow as tf
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+from activation_models.generators import get_activation_models
+from custom_model_tools.intrinsic_dim import IntrinsicDimImageNet21k, IntrinsicDimImageNet, \
+    IntrinsicDimObject2Vec, IntrinsicDimMajajHong2015
+from utils import timed
+
+
+@timed
+def main(dataset, data_dir, pooling, debug=False):
+    intrinsic_dim_df = pd.DataFrame()
+    for model, layers in get_activation_models():
+        intrinsic_dim = get_intrinsic_dim(dataset, data_dir, model, pooling)
+        intrinsic_dim.fit(layers)
+        intrinsic_dim_df = intrinsic_dim_df.append(intrinsic_dim.as_df())
+        if debug:
+            break
+    if not debug:
+        intrinsic_dim_df.to_csv(f'results/intrinsic-dim|dataset:{dataset}|pooling:{pooling}.csv', index=False)
+
+
+def get_intrinsic_dim(dataset, data_dir, activations_extractor, pooling):
+    if dataset == 'imagenet':
+        return IntrinsicDimImageNet(activations_extractor=activations_extractor,
+                                    pooling=pooling)
+    elif dataset == 'imagenet21k':
+        return IntrinsicDimImageNet21k(data_dir=data_dir,
+                                       activations_extractor=activations_extractor,
+                                       pooling=pooling)
+    elif dataset == 'object2vec':
+        return IntrinsicDimObject2Vec(data_dir=data_dir,
+                                      activations_extractor=activations_extractor,
+                                      pooling=pooling)
+    elif dataset == 'majajhong2015':
+        return IntrinsicDimMajajHong2015(activations_extractor=activations_extractor,
+                                         pooling=pooling)
+    else:
+        raise ValueError(f'Unknown intrinsic dimensionality dataset: {dataset}')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Compute and store intrinsic dimensionalities of models')
+    parser.add_argument('--dataset', type=str,
+                        choices=['imagenet', 'imagenet21k', 'object2vec', 'majajhong2015'],
+                        help='Dataset of image categories for which to compute intrinsic dimensionalities')
+    parser.add_argument('--data_dir', type=str, default=None,
+                        help='Data directory containing stimuli')
+    parser.add_argument('--no_pooling', dest='pooling', action='store_false',
+                        help='Do not perform global max-pooling prior to computing the intrinsic dimensionalities')
+    parser.add_argument('--debug', action='store_true',
+                        help='Just run a single model to make sure there are no errors')
+    args = parser.parse_args()
+
+    main(dataset=args.dataset, data_dir=args.data_dir, pooling=args.pooling, debug=args.debug)
