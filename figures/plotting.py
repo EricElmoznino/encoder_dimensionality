1d86fb7ae1b5424872c4a6993ddc87c5d2fba870
Eric Elmoznino <eric.elmoznino@gmail.com>
2023-11-04 17:54:33 -0400

    Updated figures to include correlations that take log into account

--- figures/plotting.py
+++ figures/plotting.py
@@ -1,4 +1,5 @@
 import math
+import numpy as np
 from scipy import stats
 import scipy.cluster.hierarchy as spc
 import seaborn as sns
@@ -7,31 +8,51 @@ from matplotlib.gridspec import GridSpec
 from matplotlib.patches import Patch
 
 
-def annotate_corr_grid(g, x, y, method='pearson'):
+def annotate_corr_grid(g, x, y, method="pearson"):
     g.map_dataframe(annotate_corr, x=x, y=y, method=method)
 
 
-def annotate_corr(data, x, y, ax=None, method='pearson', pos=(0.05, 0.9), color=None, **kwargs):
-    if method == 'pearson':
-        r, p = stats.pearsonr(data[x], data[y])
-    elif method == 'spearman':
-        r, p = stats.spearmanr(data[x], data[y])
+def annotate_corr(
+    data,
+    x,
+    y,
+    ax=None,
+    logx=False,
+    logy=False,
+    method="pearson",
+    pos=(0.05, 0.9),
+    color=None,
+    **kwargs,
+):
+    x = np.log(data[x]) if logx else data[x]
+    y = np.log(data[y]) if logy else data[y]
+    if method == "pearson":
+        r, p = stats.pearsonr(x, y)
+    elif method == "spearman":
+        r, p = stats.spearmanr(x, y)
     else:
         raise NotImplementedError()
     if ax is None:
         ax = plt.gca()
     if p <= 0.0001:
-        p = '****'
+        p = "****"
     elif p <= 0.001:
-        p = '***'
+        p = "***"
     elif p <= 0.01:
-        p = '**'
+        p = "**"
     elif p <= 0.05:
-        p = '*'
+        p = "*"
     else:
-        p = 'ns'
-    ax.text(pos[0], pos[1], f'r = {r:.2f}{p}'.lstrip('0'), color=color,
-            weight='bold', style='italic', transform=ax.transAxes)
+        p = "ns"
+    ax.text(
+        pos[0],
+        pos[1],
+        f"r = {r:.2f}{p}".lstrip("0"),
+        color=color,
+        weight="bold",
+        style="italic",
+        transform=ax.transAxes,
+    )
 
 
 def plot_eigenspectrum(data, x, y, hue=None, ax=None, log_scale=True, **kwargs):
@@ -39,13 +60,22 @@ def plot_eigenspectrum(data, x, y, hue=None, ax=None, log_scale=True, **kwargs):
     ax.set_xlim(data[x].min(), data[x].max())
     ax.set_ylim(data[y].min(), data[y].max())
     if log_scale:
-        ax.set_xscale('log')
-        ax.set_yscale('log')
+        ax.set_xscale("log")
+        ax.set_yscale("log")
     return ax
 
 
-def plot_metrics_categorical(kind, data, x, ys, labels=None, col_wrap=5,
-                             fig_kwargs=None, plt_kwargs=None, grid_kwargs=None):
+def plot_metrics_categorical(
+    kind,
+    data,
+    x,
+    ys,
+    labels=None,
+    col_wrap=5,
+    fig_kwargs=None,
+    plt_kwargs=None,
+    grid_kwargs=None,
+):
     if fig_kwargs is None:
         fig_kwargs = {}
     if plt_kwargs is None:
@@ -65,13 +95,13 @@ def plot_metrics_categorical(kind, data, x, ys, labels=None, col_wrap=5,
         ax = fig.add_subplot(gs[row, col])
         axs.append(ax)
 
-        if kind == 'bar':
+        if kind == "bar":
             sns.barplot(data=data, x=x, y=y, ax=ax, order=x_order, **plt_kwargs)
-        elif kind == 'violin':
+        elif kind == "violin":
             sns.violinplot(data=data, x=x, y=y, ax=ax, order=x_order, **plt_kwargs)
-        elif kind == 'box':
+        elif kind == "box":
             sns.boxplot(data=data, x=x, y=y, ax=ax, order=x_order, **plt_kwargs)
-        elif kind == 'strip':
+        elif kind == "strip":
             sns.stripplot(data=data, x=x, y=y, ax=ax, order=x_order, **plt_kwargs)
 
         ax.set(xticklabels=[], xlabel=None, ylabel=None)
@@ -81,16 +111,27 @@ def plot_metrics_categorical(kind, data, x, ys, labels=None, col_wrap=5,
             ax.set_title(labels[i])
 
     if x is not None:
-        patches = [Patch(color=sns.color_palette()[i], label=t) for i, t in enumerate(x_order)]
-        fig.legend(handles=patches, loc='upper left', bbox_to_anchor=(1.05, 0.65))
+        patches = [
+            Patch(color=sns.color_palette()[i], label=t) for i, t in enumerate(x_order)
+        ]
+        fig.legend(handles=patches, loc="upper left", bbox_to_anchor=(1.05, 0.65))
 
     fig.tight_layout()
 
     return fig, axs
 
 
-def plot_metrics_trend(data, x, ys, hue, labels=None, col_wrap=5,
-                       fig_kwargs=None, plt_kwargs=None, grid_kwargs=None):
+def plot_metrics_trend(
+    data,
+    x,
+    ys,
+    hue,
+    labels=None,
+    col_wrap=5,
+    fig_kwargs=None,
+    plt_kwargs=None,
+    grid_kwargs=None,
+):
     if fig_kwargs is None:
         fig_kwargs = {}
     if plt_kwargs is None:
@@ -119,22 +160,32 @@ def plot_metrics_trend(data, x, ys, hue, labels=None, col_wrap=5,
         ax.legend().remove()
 
     handles, labels = ax.get_legend_handles_labels()
-    fig.legend(handles=handles, labels=labels, loc='upper left', bbox_to_anchor=(1.05, 0.65))
+    fig.legend(
+        handles=handles, labels=labels, loc="upper left", bbox_to_anchor=(1.05, 0.65)
+    )
 
     fig.tight_layout()
 
     return fig, axs
 
 
-def plot_corr_heatmap(data, ax, method='pearson', reorder=True):
+def plot_corr_heatmap(data, ax, method="pearson", reorder=True):
     c = data.corr(method=method)
     if reorder:
-        linkage = spc.linkage(c, method='centroid')
+        linkage = spc.linkage(c, method="centroid")
         z = spc.dendrogram(linkage, no_plot=True)
-        idx = z['leaves']
+        idx = z["leaves"]
         c = c.iloc[idx, :].iloc[:, idx]
     cmap = sns.diverging_palette(230, 20, as_cmap=True)
-    ax = sns.heatmap(c, vmin=-1, vmax=1, cmap=cmap, square=True,
-                     xticklabels=True, yticklabels=True, ax=ax)
-    ax.set_xticklabels(ax.get_xticklabels(), rotation=45, ha='right')
+    ax = sns.heatmap(
+        c,
+        vmin=-1,
+        vmax=1,
+        cmap=cmap,
+        square=True,
+        xticklabels=True,
+        yticklabels=True,
+        ax=ax,
+    )
+    ax.set_xticklabels(ax.get_xticklabels(), rotation=45, ha="right")
     return ax
